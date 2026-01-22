014fc84e4f381cae8436efef52e2455cdd0f22a9
HasperDhan <hasperthegreat04@gmail.com>
2026-01-22 21:51:06 +0800

    Force HTTPS for assets in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
